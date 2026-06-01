4d45746c2c3157df7f737b5a56bb1029fef92b72
ishantsub <deadpoo@tutamail.com>
2026-06-02 02:56:08 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 
 var config = {
     lines: [
-        "Dear cutie pie ,",
-        " Dear chootu, App zikar bayan kro hum uska bhi sawad rakh lenge.",
-"App kux na kaho hum use bhi sun lenge app bol kr bhul jao hum use bhi samal lenge payar me hu janab zikar ka swaad swalo pr aur dar isharo me rakhta hu ishq apka ho ya mera sab kirdaro me rakhta hu..... ",
+        "Dear Dneha ji,",
+        " Dear sneha ji, I am really sorry for what i did last night i shouldn't have done that i know I did ragebaited you I'm really sorry about it I say sorry but tha really didn't stay in my words I am learning maybe I am not trying that much to improve sorry I was not able to learn because i don't know from where to start but from now onwards I will not say anything that hurt you I will try my bestsorry to hurt you it was not intentionally fir hurting you but I did every time I was treating you wrong it make feel like I treat you like shit I will repeat it I will learn it but I will not to anything that make feel disrespect.. ",
     ],
-    date:"2026-03-26T20:21:00",
+    date:"2026-06-01T20:21:00",
     names: ["☃︎【 Your ISHU 】","‎ "]
 }
