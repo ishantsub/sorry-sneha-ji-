104db3791cb528047d9ca43bc7209b4cc038ea57
ishantsub <deadpoo@tutamail.com>
2026-06-02 07:38:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 var config = {
     lines: [
-        "Dear Dneha ji,",
+        "Dear Sneha ji,",
         " Dear sneha ji, I am really sorry for what i did last night i shouldn't have done that i know I did ragebaited you I'm really sorry about it I say sorry but tha really didn't stay in my words I am learning maybe I am not trying that much to improve sorry I was not able to learn because i don't know from where to start but from now onwards I will not say anything that hurt you I will try my bestsorry to hurt you it was not intentionally fir hurting you but I did every time I was treating you wrong it make feel like I treat you like shit I will repeat it I will learn it but I will not to anything that make feel disrespect.. ",
     ],
     date:"2026-06-01T20:21:00",
